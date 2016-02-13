1d4bc191a286888dba1214bc2dd8f9f4e93c9cdb
Matt Postema <errantconstruct@gmail.com>
2016-02-13 14:45:50 -0500

    Stubbing out classes for REST

---
+++ src/main/java/com/mattpostema/pindrop/model/Response.java
@@ -0,0 +1,7 @@
+package com.mattpostema.pindrop.model;
+
+/**
+ * Created by mpostema on 2/13/16.
+ */
+public abstract class Response {
+}
---
+++ src/main/java/com/mattpostema/pindrop/rest/EntryController.java
@@ -0,0 +1,14 @@
+package com.mattpostema.pindrop.rest;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mpostema on 2/13/16.
+ */
+@RestController
+public class EntryController {
+    private Logger log = Logger.getLogger(EntryController.class);
+
+
+}
