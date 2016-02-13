69495a11d45a4fcb9f53e7979b467a52f9491194
Matt Postema <errantconstruct@gmail.com>
2016-02-13 15:03:36 -0500

    Added stubs for request methods

--- src/main/java/com/mattpostema/pindrop/model/EntriesResponse.java
+++ src/main/java/com/mattpostema/pindrop/model/EntriesResponse.java
@@ -1,7 +1,9 @@
 package com.mattpostema.pindrop.model;
 
 /**
+ * Response for a collection of phone number entries
  * Created by mpostema on 2/13/16.
  */
 public class EntriesResponse extends Response {
+
 }
--- src/main/java/com/mattpostema/pindrop/rest/EntryController.java
+++ src/main/java/com/mattpostema/pindrop/rest/EntryController.java
@@ -1,14 +1,29 @@
 package com.mattpostema.pindrop.rest;
 
+import com.mattpostema.pindrop.model.EntriesResponse;
 import org.apache.log4j.Logger;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * Controller for entry API actions
  * Created by mpostema on 2/13/16.
  */
 @RestController
+@RequestMapping(value = "/entries", produces = MediaType.APPLICATION_JSON_VALUE)
 public class EntryController {
     private Logger log = Logger.getLogger(EntryController.class);
 
+    @RequestMapping(method = RequestMethod.GET)
+    public EntriesResponse getAllEntries() {
+        return new EntriesResponse();
+    }
 
+    @RequestMapping(value = "/{areaCode}", method = RequestMethod.GET)
+    public EntriesResponse getEntriesByAreaCode(@PathVariable String areaCode) {
+        return new EntriesResponse();
+    }
 }
